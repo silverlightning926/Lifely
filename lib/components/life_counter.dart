b5ce0423606011fbec91aca7a35736253822f7f1
Sidd Rao <siddrao.926@gmail.com>
2022-08-26 00:24:51 +0530

    Created Basic Life Counter Component

---
+++ lib/components/life_counter.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LifeCounter extends StatefulWidget {
+  const LifeCounter({
+    Key? key,
+    required this.color,
+    required this.playerNum,
+    required this.quarterRotations,
+    required this.startingLife,
+  }) : super(key: key);
+
+  final Color color;
+  final int playerNum;
+  final int quarterRotations;
+  final int startingLife;
+
+  @override
+  State<LifeCounter> createState() => _LifeCounterState();
+}
+
+class _LifeCounterState extends State<LifeCounter> {
+  int currentLife = 20;
+
+  void resetCounter() {
+    setState(() {
+      currentLife = 0;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentLife = widget.startingLife;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: RotatedBox(
+        quarterTurns: widget.quarterRotations,
+        child: Container(
+          decoration: BoxDecoration(
+            color: widget.color,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      "P${widget.playerNum}",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 25,
+                        letterSpacing: 3,
+                      ),
+                    ),
+                    Text(
+                      "$currentLife",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 100,
+                      ),
+                    ),
+                    const Icon(
+                      CupertinoIcons.heart_solid,
+                      size: 50,
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                      onPressed: () {
+                        setState(() {
+                          currentLife++;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      onPressed: () {
+                        setState(() {
+                          currentLife--;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
